14fa88b9227cf3d094dac74a1f2c521322f0a18e
ej-rarus <lpl2001@naver.com>
2024-09-06 10:42:09 +0900

    api 정보 호출 테스트

---
+++ get_test.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    // /data 경로에서 JSON 데이터를 받아옴
+    fetch('http://localhost:3000/json')
+        .then(response => response.json()) // 응답을 JSON으로 변환
+        .then(data => {
+            // 데이터를 HTML 요소에 출력
+            console.log(data);
+            document.getElementById('id').textContent = data.id;
+            document.getElementById('name').textContent = data.name;
+            document.getElementById('email').textContent = data.email;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+};
