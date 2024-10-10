fb2a88279ec8dd246718ea833f7525ecc15a2f5b
ej-rarus <lpl2001@naver.com>
2024-10-10 10:54:51 +0900

    public 폴더 추가

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = 3001;
 app.use(cors());
 // 라우터
 
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'public')));
+
 // 기본 경로에 대한 요청 처리
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
